efa0b39c581da3e6221fe7911ea20f522c5526c1
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-01-04 22:34:37 +0700

    Implement sms receiver permission

--- app/src/main/java/io/erikrios/github/mybroadcastreceiver/MainActivity.kt
+++ app/src/main/java/io/erikrios/github/mybroadcastreceiver/MainActivity.kt
@@ -1,11 +1,17 @@
 package io.erikrios.github.mybroadcastreceiver
 
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import io.erikrios.github.mybroadcastreceiver.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    companion object {
+        private const val SMS_REQUEST_CODE = 101
+    }
 
     private var binding: ActivityMainBinding? = null
 
@@ -21,6 +27,28 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onClick(v: View?) {
         when (v?.id) {
 
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == SMS_REQUEST_CODE) {
+            when {
+                grantResults[0] == PackageManager.PERMISSION_GRANTED -> Toast.makeText(
+                    this,
+                    "Sms receiver permission diterima",
+                    Toast.LENGTH_SHORT
+                ).show()
+                else -> Toast.makeText(
+                    this,
+                    " Sms receiver permission ditiolak",
+                    Toast.LENGTH_SHORT
+                )
+                    .show()
+            }
         }
     }
 
